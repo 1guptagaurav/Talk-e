66bbf563248d4859f336dfb9c2ea87b790b0d719
Gaurav Gupta <gauravgupta1197@gmail.com>
2024-08-31 18:11:55 +0530

    add context Api

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,15 @@ import { CloseButton } from "@chakra-ui/react";
 import "./App.css";
 import {Route} from "react-router-dom"
 import {HomePage} from "./Pages/Homepage.jsx"
+import { ChatProvider } from './Context/ContextApi.jsx';
 function App() {
 
   return (
+    <ChatProvider>
       <div className="App">
-       <HomePage/>
+        <HomePage/>
       </div>
+    </ChatProvider>
   );
 }
 
---
+++ Frontend/src/Context/ContextApi.jsx
@@ -0,0 +1,9 @@
+import { createContext,useContext } from "react";
+
+export const ChatContext=createContext()
+
+export const ChatProvider=ChatContext.Provider
+
+export default function useChat(){
+    return useContext(ChatContext)
+}
