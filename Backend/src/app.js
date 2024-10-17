5621fc84e1cdf6d39b471349dbdffd199915199d
Gaurav Gupta <gauravgupta1197@gmail.com>
2024-10-17 20:52:11 +0530

    add socket io

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,12 @@ import chatRouter from "./routes/chat.routes.js"
 import messageRouter from "./routes/message.routes.js";
 const app=express()
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
